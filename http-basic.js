dd6f75653b2385b3428bb895b47d5b8860cb98bc
David Huang <bugman195@gmail.com>
2020-10-27 17:02:11 +0800

    Add http basic

---
+++ http-basic.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello world');
+    res.end();
+  } else {
+    res.write('Use other domain pls');
+    res.end();
+  }
+});
+
+server.listen('3000');
