712f0031f822dc44906f6d91e40ee3e3f9143921
David Huang <bugman195@gmail.com>
2020-10-27 15:42:06 +0800

    Add emitter practice

---
+++ emitter.js
@@ -0,0 +1,27 @@
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial', (num1, num2) => {
+  console.log(num1 + num2);
+});
+
+eventEmitter.emit('tutorial', 1, 2);
+
+class Person extends EventEmitter {
+  constructor(name) {
+    super();
+
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+}
+
+let pedro = new Person('Pedro');
+pedro.on('name', () => {
+  console.log('my name is ' + pedro.name);
+});
+
+pedro.emit('name');
